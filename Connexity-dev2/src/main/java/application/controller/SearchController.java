c4b4e8f7a5a3873c55fdc9299885d70f5bc7faee
junli9393 <junli9393@gmail.com>
2016-05-01 08:33:46 -0700

    home->query

---
+++ Connexity-dev2/src/main/java/application/controller/SearchController.java
@@ -0,0 +1,28 @@
+package application.controller;
+
+import application.model.Offer;
+import application.search.SearchStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/search")
+public class SearchController {
+
+    @Autowired
+    private SearchStrategy searchStrategy;
+
+    @RequestMapping("/*")
+    public String search(@RequestParam(value = "key", required = false, defaultValue = "World") String key, Model model) throws IOException{
+        List<Offer> searchResult = searchStrategy.search(key);
+        model.addAttribute("offers", searchResult);
+        return "query";
+    }
+
+}
