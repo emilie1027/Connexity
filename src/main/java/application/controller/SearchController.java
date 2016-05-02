83c592b375f947f5488f80688b50d58f6b0811ef
emilie <bupt.emilie@gmail.com>
2016-05-01 22:27:50 -0700

    add custmozied ranking function, redefine Query function

--- src/main/java/application/controller/SearchController.java
+++ src/main/java/application/controller/SearchController.java
@@ -11,38 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
-
+    
     @Autowired
     private SearchStrategy searchStrategy;
     private List<Offer> searchResult;
-    private String currentKey;
-
+    private Query currentQuery;
+    
     public SearchController() {
-        searchResult = new ArrayList<Offer>();
-        currentKey = "";
+        searchResult = null;
+        currentQuery = null;
     }
-
+    
     @RequestMapping("/*")
     public String search(@RequestParam(value = "key", required = false, defaultValue = "World") String key, Model model) throws IOException {
         searchResult = searchStrategy.basicSearch(key);
-        currentKey = key;
         model.addAttribute("offers", searchResult);
         return "query";
     }
-
-    public String search(@ModelAttribute("query") Query query, Model model) throws IOException{
-        if (query.getKey() == currentKey) {
-            return "query";
-        }
-        else {
-            return search(query.getKey(), model);
-        }
+    
+    public String search(@ModelAttribute("Query") Query query, Model model) throws IOException{
+        return search(query.getSingleParam("keyword"), model);
     }
-
+    
 }
